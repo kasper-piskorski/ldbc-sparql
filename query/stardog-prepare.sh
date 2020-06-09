99301edf28c25920786eae31f1ea7b3b24330a98
Kasper Piskorski <k.piskorski@o2.pl>
2020-06-09 18:04:11 +0200

    Update stardog-prepare.sh

--- query/stardog-prepare.sh
+++ query/stardog-prepare.sh
@@ -1,7 +1,5 @@
-TIMEOUT_MS=$(( SPARQL_TIMEOUT_MINS * 60 * 1000 ))
-echo $TIMEOUT_MS
-cp ../load/stardog.properties ${STARDOG_HOME}stardog.properties
 ${STARDOG_HOME}bin/stardog-admin server stop
+cp ../load/stardog.properties ${STARDOG_HOME}stardog.properties
 sed -i "s|# query.timeout.*|query.timeout=${SPARQL_TIMEOUT_MINS}m|g" ${STARDOG_HOME}stardog.properties
 sed -i "s|strict.parsing.*|strict.parsing=false|g" ${STARDOG_HOME}stardog.properties
 sed -i "s|memory.mode.*|memory.mode=read_optimized|g" ${STARDOG_HOME}stardog.properties
