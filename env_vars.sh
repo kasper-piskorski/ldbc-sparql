8c69f2d98ca8dd7a1abfbd2749d208fd5855ad94
Kasper Piskorski <k.piskorski@o2.pl>
2020-07-01 17:04:49 +0200

    Clean dirs

--- env_vars.sh
+++ env_vars.sh
@@ -3,14 +3,14 @@ export SPARQL_SEEDS=5
 export SPARQL_TIMEOUT_MINS=5
 export SFACTOR=sf1
 export SPLIT_TRIPLE_SIZE=500M
-export SEED_PATH=/Users/kasper/data/$SFACTOR-ttl/substitution_parameters/
-export RDF_DATA_DIR=/Users/kasper/data/$SFACTOR-ttl/social_network/
+export SEED_PATH=
+export RDF_DATA_DIR=
 
-export VIRTUOSO_HOME=/Applications/Virtuoso\ Open\ Source\ Edition\ v7.2.app/Contents/virtuoso-opensource/
+export VIRTUOSO_HOME=
 export VIRTUOSO_AVAILABLE_MEMORY_GB=8
 export VIRTUOSO_DB_SIZE_GB=10
 export VIRTUOSO_LOADERS=4
 
-export STARDOG_HOME=/Users/kasper/dev/stardog-7.3.0/
+export STARDOG_HOME=
 export STARDOG_SERVER_JAVA_ARGS="-Xms8G -Xmx8G -XX:MaxDirectMemorySize=16G"
 
