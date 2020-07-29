752be0f6f3f0e4c5283d721736192f21d97e5794
Kacper Piskorski <k.piskorski@o2.pl>
2020-07-29 10:36:13 +0100

    revert some changes

--- env_vars.sh
+++ env_vars.sh
@@ -1,18 +1,16 @@
-export SPARQL_BACKEND=rdfox
+export SPARQL_BACKEND=stardog
 export SPARQL_SEEDS=5
 export SPARQL_TIMEOUT_MINS=5
-export SFACTOR=sf01
+export SFACTOR=sf1
 export SPLIT_TRIPLE_SIZE=500M
-export SEED_PATH=~/data/${SFACTOR}-ttl/substitution_parameters/
-export RDF_DATA_DIR=~/data/${SFACTOR}-ttl/social_network/
+export SEED_PATH=
+export RDF_DATA_DIR=
 
-export VIRTUOSO_HOME=/Applications/Virtuoso\ Open\ Source\ Edition\ v7.2.app/Contents/virtuoso-opensource/
+export VIRTUOSO_HOME=
 export VIRTUOSO_AVAILABLE_MEMORY_GB=8
 export VIRTUOSO_DB_SIZE_GB=10
 export VIRTUOSO_LOADERS=4
 
 export STARDOG_HOME=
 export STARDOG_SERVER_JAVA_ARGS="-Xms8G -Xmx8G -XX:MaxDirectMemorySize=16G"
-
-export RDFOX_HOME=~/dev/RDFox-macOS-3.0.1/
 
